7027b421301884480818df35753105d56f823e07
cs-system574 <126313741+cs-system574@users.noreply.github.com>
2023-02-26 09:50:09 +0900

    Add files via upload

--- home/CS.js
+++ home/CS.js
@@ -1,12 +1,30 @@
-window.onload= function(){
+window.onload= async function(){
     if (window.matchMedia('(min-width:768px)').matches) {
         location="../error/index.html"
     }
+    fetch(`https://script.google.com/macros/s/AKfycbxeci6Wtt-jd72sGCTIo7UedQ1eIwwEDrZdDM4bO4Xh8cM59i-po-za9Us9u-1l7r9qVQ/exec?number=${localStorage.getItem("cs_id")}`)
+    .then(res=>res.json())
+    .then(json=>{
+        if(json.rabbitwhole) {
+            document.getElementById("rabbit").insertAdjacentHTML("afterbegin",`<img src="./img/兎穴.png"">`)
+        }
+        if(json.witch){
+            if(localStorage.getItem("cs_id")=="a1007") $('body').css({backgroundImage:`url("./img/スマホ画面アリス.png")`})
+            if(localStorage.getItem("cs_id")=="1acenoel") $('body').css({backgroundImage:`url("./img/スマホ画面ドロシー.png")`})
+        }
+        else $('body').css({backgroundImage:`url("./img/スマホ画面.png")`})
+    })
 }
 
 $(function () {
-    $('#story').click(function(){
-        $('#modalArea').fadeIn();
+    $('#story').click(async function(){
+        await fetch(`https://script.google.com/macros/s/AKfycbxeci6Wtt-jd72sGCTIo7UedQ1eIwwEDrZdDM4bO4Xh8cM59i-po-za9Us9u-1l7r9qVQ/exec?number=${localStorage.getItem("cs_id")}`)
+        .then(res=>res.json())
+        .then(json=>{
+            if(json.story) alert("今日の分は既に書いています。")
+            else $('#modalArea').fadeIn();
+        })
+        
     });
     $('#closeModal , #modalBg').click(function(){
       $('#modalArea').fadeOut();
@@ -20,6 +38,8 @@ $(function () {
     $('#yes').click(function(){
         $('#modalArea').fadeOut();
         $('#check').fadeOut();
+        const diary=document.getElementById("today").value;
+        fetch(`https://script.google.com/macros/s/AKfycbzKjZdIxFOUuINwdM5a-iZIOdW-9ofVQOKOez0PixO0OXmkJ4m5oOAqHUicrx4asLl2/exec?number=${localStorage.getItem("cs_id")}?${diary}`)
     });
 
     $('#densyou').click(function(){
@@ -30,14 +50,14 @@ $(function () {
     })
 
     $('#history').click(async function(){
-        await fetch(`https://script.google.com/macros/s/AKfycbzoqx0tP72SNyCz5KfuKfMJlqEP58jomNScZtSdXB4daVZb5TaNW_FmH9r3kOcgNBlMvQ/exec?number=${localStorage.getItem("cs_id")}`)
+        await fetch(`https://script.google.com/macros/s/AKfycbxeci6Wtt-jd72sGCTIo7UedQ1eIwwEDrZdDM4bO4Xh8cM59i-po-za9Us9u-1l7r9qVQ/exec?number=${localStorage.getItem("cs_id")}`)
         .then(res=>res.json())
         .then(json=>{
             const date=["〇","×","▽","□","☆"];
             const day=["月","火","水","木","金"];
             let text="";
             json.diary.forEach((element,index) => {
-                text+=`<div>6月${date[index]}日(${day[index]})</div><div>　${element}</div>`
+                text+=`<div class="nikki">6月${date[index]}日(${day[index]})</div><div class="nikki">　${element}</div>`
             });
             if(text=="") text=`<div id="no_diary">まだ何も書いていません。</div>`
             return text
@@ -46,8 +66,17 @@ $(function () {
             const historylist=document.getElementById("historylist")
             if(historylist.childElementCount==1)
                 historylist.insertAdjacentHTML("beforeend",text);
-            if(historylist.childElementCount==2){
+            else if(historylist.childElementCount==2){
                 document.getElementById("no_diary").remove();
+                historylist.insertAdjacentHTML("beforeend",text)
+            }
+            else{
+                let children = document.getElementsByClassName('nikki');
+                //子要素数取得
+                let len = children.length;
+                for(let i = 0; i < len; i++){
+                    historylist.removeChild(children[0]);
+                }
                 historylist.insertAdjacentHTML("beforeend",text)
             }
         })
--- login/script.js
+++ login/script.js
@@ -8,7 +8,7 @@ window.onload= function(){
 async function send(){
     const id=document.getElementById("invite_num").value;
     if(id=="a1007"||id=="1acenoel"||id=="0rehly20"||id=="9avhw312"){
-        await fetch(`https://script.google.com/macros/s/AKfycbzoqx0tP72SNyCz5KfuKfMJlqEP58jomNScZtSdXB4daVZb5TaNW_FmH9r3kOcgNBlMvQ/exec?number=${id}`)
+        await fetch(`https://script.google.com/macros/s/AKfycbxeci6Wtt-jd72sGCTIo7UedQ1eIwwEDrZdDM4bO4Xh8cM59i-po-za9Us9u-1l7r9qVQ/exec?number=${id}`)
         .then(res=>res.json())
         .then(data=>{
             if(data.use) alert("その招待コードはすでに使用されています");
